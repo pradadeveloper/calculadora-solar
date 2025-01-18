2b3889ff3de250ab54ce7fc92a1ddf45aebee9a6
unknown <juanfelipepradaing@gmail.com>
2025-01-18 18:46:21 -0500

    Configuración inicial

--- app.py
+++ app.py
@@ -19,11 +19,3 @@ if __name__ == '__main__':
     app.run(debug=True)
     
 
-# Back-end en Python con Flask (para la lógica de la suma).
-
-# Front-end simple en HTML para interactuar con el usuario.
-
-# Despliegue en internet utilizando Render o PythonAnywhere.
-
-
-# Explicación: La ruta '/' muestra el formulario HTML. Cuando el usuario envía los datos, Flask recibe los valores a y b, realiza la suma y envía el resultado a la página.
