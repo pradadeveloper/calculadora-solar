f197ed2236e8fcd39f874b05b4f51cbb1b55ffc2
unknown <juanfelipepradaing@gmail.com>
2025-01-21 15:02:07 -0500

    estructura de landing home

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-
+# Ruta principal (Landing Page)
+@app.route('/')
 def index():
-    resultado = None
-    if request.method == 'POST':
-        try:
-            a = float(request.form['a'])
-            b = float(request.form['b'])
-            resultado = a + b
-        except ValueError:
-            resultado = "Error ingresa números validos"
-    return render_template('index.html', resultado=resultado)
+    return render_template('index.html')
+
+# Ruta para "Cotizador de proyectos solares"
+@app.route('/calculadora_solar')
+def calculadora_solar():
+    return render_template('calculadora_solar.html')
+
+# Ruta para "Cotizador de equipos solares"
+@app.route('/calculadora_equipos')
+def calculadora_equipos():
+    return render_template('calculadora_equipos.html')
+
+# Ruta para "Diligenciamiento de contratos"
+@app.route('/diligenciamiento_contratos')
+def diligenciamiento_contratos():
+    return render_template('diligenciamiento_contratos.html')
+
+# Ruta para "Servicios de instalaciones terceros"
+@app.route('/instalaciones_terceros')
+def instalaciones_terceros():
+    return render_template('instalaciones_terceros.html')
+
+# Ruta para "Diligenciamiento de SAGRILAFT"
+@app.route('/sagrilaft')
+def sagrilaft():
+    return render_template('sagrilaft.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
     
 
